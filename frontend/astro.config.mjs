794e087fbdb95f1d8e8188af073671d213359e74
Zhu Zhanyan <program.nom@gmail.com>
2025-02-03 16:19:36 +0800

    chore: update site root to github actions

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ntudevhub.org",
+  site: "https://devhub-ilab.github.io",
   integrations: [
     react(),
     tailwind({
